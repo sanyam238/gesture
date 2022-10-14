20c500677e9ac559cbbc42e1bb477cfbf9b617cc
sanyam238 <82820362+sanyam238@users.noreply.github.com>
2022-10-14 19:32:01 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -17,3 +17,24 @@ function take_snapshot(){
 console.log("ml5 version:",ml5.version);
 
 classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/MNzFHFBXn/model.json");
+
+function modelLoaded(){
+    console.log("model loaded !")
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is - "+prediction_1;
+    utter_this = new SpeechSynthesisUtterance(speak_data_1);
+    synth.speak(utter_this);
+}
+
+function gesture(){
+    img = document.getElementById("captured_image");
+    classifier.classify(img,gotResult)
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
